205132caf9dfe5c3cba2dfb00dec54e3bb5d231a
Xuan-Zhang Gong <gongxuanzhangmelt@gmail.com>
2025-02-25 10:00:33 +0800

    update to unmodifiable set

--- server-common/src/main/java/org/apache/kafka/server/config/ServerLogConfigs.java
+++ server-common/src/main/java/org/apache/kafka/server/config/ServerLogConfigs.java
@@ -19,7 +19,6 @@ package org.apache.kafka.server.config;
 
 import org.apache.kafka.common.config.TopicConfig;
 
-
 import static org.apache.kafka.server.config.ServerTopicConfigSynonyms.LOG_PREFIX;
 
 /**
@@ -162,5 +161,4 @@ public class ServerLogConfigs {
     public static final Long LOG_DIR_FAILURE_TIMEOUT_MS_DEFAULT = 30000L;
     public static final String LOG_DIR_FAILURE_TIMEOUT_MS_DOC = "If the broker is unable to successfully communicate to the controller that some log " +
         "directory has failed for longer than this time, the broker will fail and shut down.";
-
 }
